747e9ce4618f5d189da76afad8282a2d48b9c619
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-08-03 21:46:07 -0700

    changed site to label for save option

--- utilities/savePassword.js
+++ utilities/savePassword.js
@@ -3,8 +3,8 @@ const path = require('path')
 const os = require('os')
 const chalk = require('chalk')
 
-const savePassword = (password, site) => {
-    const text = typeof site === 'string' ? `${site}: ${password}` : password
+const savePassword = (password, label) => {
+    const text = typeof site === 'string' ? `${label}: ${password}` : password
     fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (e, id) => {
         fs.write(id, text + os.EOL, null, 'utf-8', () => {
             fs.close(id, () => {
